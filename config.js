d4909f4e4c7a7a8a68c0915f7ac2c47f2b4f3cea
omeshchintha <omeshchintha95@gmail.com>
2025-09-09 19:04:48 +0530

    Add this functionalities, and modified code

--- config.js
+++ config.js
@@ -140,15 +140,16 @@
 //   : "http://10.10.148.25/";    // Server run
 
 var SPEEDTEST_SERVERS = [
+
    {
     name: "Main CDN IP",
-    server: "//103.189.178.120//",   // correct format
+    server: "//103.189.178.121/streamtv/",   // correct format
     dlURL: "backend/garbage.php",
     ulURL: "backend/empty.php",
     pingURL: "backend/empty.php",
     getIpURL: "backend/getIP.php"
   },
-  {
+    {
     name: "CDN DSN3",
     server: "//10.6.6.165/",
     dlURL: "backend/garbage.php",
@@ -156,7 +157,7 @@ var SPEEDTEST_SERVERS = [
     pingURL: "backend/empty.php",
     getIpURL: "backend/getIP.php"
   },
-  {
+    {
     name: "BLCRDHE EDGECDN1006",
     server: "//10.7.7.252/",
     dlURL: "backend/garbage.php",
@@ -164,15 +165,15 @@ var SPEEDTEST_SERVERS = [
     pingURL: "backend/empty.php",
     getIpURL: "backend/getIP.php"
   },
-  {
-    name: "EDGECDN KAMALAMILLSHATHWAY1007",
+   {
+    name: "KAMALAMILLSHATHWAY1007",
     server: "//172.31.42.2/",
     dlURL: "backend/garbage.php",
     ulURL: "backend/empty.php",
     pingURL: "backend/empty.php",
     getIpURL: "backend/getIP.php"
   },
-  {
+    {
     name: "HYDERABAD EDGECDN1008",
     server: "//172.31.32.2/",
     dlURL: "backend/garbage.php",
@@ -180,12 +181,29 @@ var SPEEDTEST_SERVERS = [
     pingURL: "backend/empty.php",
     getIpURL: "backend/getIP.php"
   },
-  {
-    name: "KANPUR EXCITEL EDGECDN1010",
+   {
+    name: "KANPUR EXCITEL",
     server: "//172.29.3.178/",
     dlURL: "backend/garbage.php",
     ulURL: "backend/empty.php",
     pingURL: "backend/empty.php",
     getIpURL: "backend/getIP.php"
-  }
+  },
+
+     {
+    name: "BSNL HYDERABAD",
+    server: "//192.168.80.50/",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+      {
+    name: "Testing",
+    server: "//192.168.12.53/",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
 ];
